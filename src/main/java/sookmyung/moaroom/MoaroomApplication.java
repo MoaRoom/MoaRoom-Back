3eb6ef3cb9539137bffdcb1191471b60b302fabc
mjms0214 <mjms0214@sookmyung.ac.kr>
2023-03-31 16:53:29 +0900

    Inital Commit

---
+++ src/main/java/sookmyung/moaroom/MoaroomApplication.java
@@ -0,0 +1,13 @@
+package sookmyung.moaroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoaroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoaroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sookmyung/moaroom/MoaroomApplicationTests.java
@@ -0,0 +1,13 @@
+package sookmyung.moaroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoaroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
