d37c8dfa68368e142d8a507b9269bd79d4128ded
mjms0214 <mjms0214@sookmyung.ac.kr>
2023-05-05 13:49:02 +0900

    [feat/#16] Refactor: snake to camel case(in Lecture Table)

--- src/main/java/sookmyung/moaroom/Service/LectureService.java
+++ src/main/java/sookmyung/moaroom/Service/LectureService.java
@@ -23,12 +23,12 @@ public class LectureService {
             System.out.println("Save");
             System.out.println(data);
             Lecture newLecture = new Lecture();
-            newLecture.setLecture_id(UUID.randomUUID());
+            newLecture.setLectureId(UUID.randomUUID());
             newLecture.setTitle(String.valueOf(data.get("title")));
 
             // professor_id에 따옴표도 포함되어 있으므로, 슬라이싱으로 제거 후 UUID로 변환해야함
             String professor_id = data.get("professor_id").toString();
-            newLecture.setProfessor_id(UUID.fromString(professor_id.substring(1,37)));
+            newLecture.setProfessorId(UUID.fromString(professor_id.substring(1,37)));
             newLecture.setRoom(data.get("room").getAsInt());
 
             lectureRepository.save(newLecture);
@@ -46,7 +46,7 @@ public class LectureService {
             if (lectureRepository.findById(UUID.fromString(lecture_id)).isEmpty()){
                 throw new Exception("존재하지 않는 강의");
             }
-            if (data.get("professor_id").equals(existLecture.getProfessor_id().toString())){
+            if (data.get("professor_id").equals(existLecture.getProfessorId().toString())){
                 throw new Exception("수정 권한이 없는 사용자");
             }
 
@@ -91,7 +91,7 @@ public class LectureService {
             if(lectureRepository.findById(UUID.fromString(id)).isEmpty()){
                 throw new Exception("존재하지 않는 강의");
             }
-            if (!lectureRepository.findById(UUID.fromString(id)).get().getProfessor_id().toString().equals(professor_id)){
+            if (!lectureRepository.findById(UUID.fromString(id)).get().getProfessorId().toString().equals(professor_id)){
                 throw new Exception("삭제 권한이 없는 사용자");
             }
 
