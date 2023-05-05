aa449a98b485d18709a54027ecdf2151755e5439
mjms0214 <mjms0214@sookmyung.ac.kr>
2023-05-05 13:48:13 +0900

    [feat/#16] Add: add column room_count(in Lecture table)

--- src/main/java/sookmyung/moaroom/Model/Lecture.java
+++ src/main/java/sookmyung/moaroom/Model/Lecture.java
@@ -4,6 +4,7 @@ import com.sun.istack.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -18,14 +19,20 @@ public class Lecture {
     @Id
     @Column(name = "lecture_id")
     @NotNull
-    private UUID lecture_id;
+    private UUID lectureId;
     @Column(name = "title")
     @NotNull
     private String title;
     @Column(name = "professor_id")
     @NotNull
-    private UUID professor_id;
+    private UUID professorId;
     @Column(name = "room")
     @NotNull
-    private  Integer room;
+    private Integer room;
+    @Column(name = "room_count")
+    @ColumnDefault("30")
+    @NotNull
+    private Integer roomCount;
+
 }
+
