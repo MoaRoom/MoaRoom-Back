2a906d70afb2390979d438d94f0858962fa4ed30
mjms0214 <mjms0214@sookmyung.ac.kr>
2023-06-01 15:13:55 +0900

    Add: CORS

--- src/main/java/sookmyung/moaroom/MoaroomApplication.java
+++ src/main/java/sookmyung/moaroom/MoaroomApplication.java
@@ -2,7 +2,10 @@ package sookmyung.moaroom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableScheduling
@@ -12,4 +15,13 @@ public class MoaroomApplication {
 		SpringApplication.run(MoaroomApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
+	}
 }
