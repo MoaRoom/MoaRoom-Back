04e964a951541987d7efceecf2e338206305af8e
mjms0214 <mjms0214@sookmyung.ac.kr>
2023-06-04 05:25:30 +0900

    [feat/#42] Add: return stepList

--- src/main/java/sookmyung/moaroom/Controller/StepController.java
+++ src/main/java/sookmyung/moaroom/Controller/StepController.java
@@ -1,11 +1,9 @@
 package sookmyung.moaroom.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sookmyung.moaroom.Dto.requestScoreDto;
+import sookmyung.moaroom.Dto.responseStepDto;
 import sookmyung.moaroom.Model.Step;
 import sookmyung.moaroom.Service.StepService;
 
@@ -19,6 +17,11 @@ public class StepController {
     @PostMapping("assignment/score")
     public void scoring(@RequestBody requestScoreDto score){
         stepService.scoring(score);
+    }
+
+    @GetMapping("step/{assignment_id}")
+    public List<responseStepDto> findStepList(@PathVariable(name = "assignment_id") String id){
+        return stepService.findStepList(id);
     }
 
     @GetMapping("step/all")
--- src/main/java/sookmyung/moaroom/Respository/StepRepository.java
+++ src/main/java/sookmyung/moaroom/Respository/StepRepository.java
@@ -5,7 +5,12 @@ import org.springframework.stereotype.Repository;
 import sookmyung.moaroom.Model.Step;
 import sookmyung.moaroom.Model.StepPK;
 
+import java.util.List;
+import java.util.UUID;
+
 
 @Repository
 public interface StepRepository extends JpaRepository<Step, StepPK> {
+
+    public List<Step> findByAssignmentId(UUID assinmentId);
 }
