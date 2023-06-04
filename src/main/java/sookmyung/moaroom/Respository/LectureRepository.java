605c6dd8e7e28b9b54a299cb449d03fe8c040e13
mjms0214 <mjms0214@sookmyung.ac.kr>
2023-06-04 15:53:37 +0900

    [feat/#48] Refactor: modify lecture delete api

--- src/main/java/sookmyung/moaroom/Respository/LectureRepository.java
+++ src/main/java/sookmyung/moaroom/Respository/LectureRepository.java
@@ -2,10 +2,13 @@ package sookmyung.moaroom.Respository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import sookmyung.moaroom.Model.Lecture;
 
 import java.util.UUID;
 
 @Repository
 public interface LectureRepository extends JpaRepository <Lecture, UUID> {
+    @Transactional
+    public void deleteByTitleAndRoom(String title, Integer room);
 }
