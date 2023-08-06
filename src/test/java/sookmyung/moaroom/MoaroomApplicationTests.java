c20805a40eed7a8f68c1f6c0cec106cf65772a88
NayeonKeum <rmaskdus0208@gmail.com>
2023-08-06 17:54:18 +0900

    [feat/#63] Add: AWS Codedeploy CI

--- src/test/java/sookmyung/moaroom/MoaroomApplicationTests.java
+++ src/test/java/sookmyung/moaroom/MoaroomApplicationTests.java
@@ -3,11 +3,7 @@ package sookmyung.moaroom;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MoaroomApplicationTests {
-
-	@Test
 	void contextLoads() {
 	}
-
 }
